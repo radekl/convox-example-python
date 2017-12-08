edd0ae7739a45b9a17d8741f5f2b15785eb141e8
Radosław Lisowski <radoslaw.lisowski@schibsted.com>
2017-12-08 11:09:49 +0100

    Added envs context

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import os
 
 # the all-important app variable:
 app = Flask(__name__)
+
+@app.route("/envs")
+def get_envs():
+    to_ret = "Hello World! <br />\n"
+    for x, y in os.environ.iteritems():
+        to_ret += x+"="+y+"<br/>\n"
+
+    return to_ret
+
 
 @app.route("/", defaults={"path":""})
 @app.route("/<path:path>")
@@ -10,3 +20,4 @@ def hello(path):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=3000)
+
